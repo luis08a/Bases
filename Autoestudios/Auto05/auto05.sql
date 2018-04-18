5631cc66073258c1f9048789ec2baed4f6d40a9f
luis08a <31138710+luis08a@users.noreply.github.com>
2018-04-18 15:43:16 -0500

    Add files via upload

---
+++ Autoestudios/Auto05/auto05.sql
@@ -0,0 +1,28 @@
+--Tabla producto
+CREATE table Product (productID number(11), name varchar(50) not null, productNumber varchar(25) not null, color varchar(15), standardCost NUMBER(10,2) not null, listPrice number(10,2) not null, sze varchar(5), weight number(8,2), productCategoryID number(11), productModelID number(11), sellStartDate date not null, sellEndDate date, discontinuedDate date, thumbnailPhotoFileName varchar(50), detail XMLType);
+
+--Primary key
+ALTER TABLE Product add constraint pk_Product primary key(productID);
+
+--Poblado
+INSERT INTO Product(productID, name, productNumber, standardCost, listPrice,sellStartDate, detail) values (1, 'Producto1','2', 5.01, 5.01, TO_DATE('2000','YYYY'), 
+'<?xml version="1.0"?>
+<detalles>
+    <edades Min ="1" Max="8">
+    </edades>
+	<materiales>
+		<material>plastico</material>
+	</materiales>
+	<tallas>
+		<talla>S</talla>
+		<talla>M</talla>
+		<talla>L</talla>
+	</tallas>
+	<garantia></garantia>
+	<peso></peso>
+	<origen></origen>
+</detalles>');        
+
+
+--Eliminazao de la tabla
+DROP TABLE Product;
